f0e12549fa1d5d1ff3dad72830b4ddafdcf12d78
img21326 <img21326@gmail.com>
2018-03-27 08:27:06 +0800

    add drop resize on report

--- resources/assets/js/components/back/report/ShowReportComponent.vue
+++ resources/assets/js/components/back/report/ShowReportComponent.vue
@@ -1,32 +1,48 @@
-
 <template>
-    <div>
-        <button @click="add()"></button>
-        <div v-for="element in myArray">
 
-        </div>
+    <div style="height: 500px; width: 100%; border: 1px solid red; position: relative;">
+        <vue-draggable-resizable :w="300" :h="300" v-on:dragging="onDrag" v-on:resizing="onResize" :parent="true" :grid="[30,30]">
+            <div class="card" style="width: 100%;height: 105%">
+                <div class="header">
+                    <h4 class="title">Team Members</h4>
+                </div>
+                <div class="content" v-bind:style="{ width: (width + 0) + 'px', height: (height-15) + 'px' }">
+                    <canvas id="mix3" count="2" :width="width " :height="height"></canvas>
+                    <chartjs-line target="mix3" :data="[70, 40, 55, 80, 55]" ></chartjs-line>
+                    <chartjs-bar target="mix3" :data="[20, 30, 20, 10, 5]" :labels="['A', 'B', 'C', 'D', 'E']"></chartjs-bar>
+                </div>
+            </div>
+
+
+        </vue-draggable-resizable>
     </div>
-
 </template>
 
 <script>
+    import VueDraggableResizable from 'vue-draggable-resizable'
+
+    Vue.component('vue-draggable-resizable', VueDraggableResizable)
 
     export default {
-        data() {
+        data: function () {
             return {
-                myArray:[]
+                width: 0,
+                height: 0,
+                x: 0,
+                y: 0
             }
-        },
-        created() {
-
         },
         methods: {
-            add(){
-                this.myArray.push('s')
+            onResize: function (x, y, width, height) {
+                this.x = x
+                this.y = y
+                this.width = width
+                this.height = height
+            },
+            onDrag: function (x, y) {
+                this.x = x
+                this.y = y
             }
-        },
-        beforeDestroy() {
-
         }
     }
-    </script>
+</script>
