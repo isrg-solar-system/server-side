412b67ee22848f56f1a4739a1db00f015d567051
img21326 <img21326@gmail.com>
2018-05-01 09:48:32 +0800

    add realtime chart

---
+++ resources/assets/js/components/front/index/realtimeComponent.vue
@@ -0,0 +1,48 @@
+<script>
+    //Importing Line class from the vue-chartjs wrapper
+    import { Line,mixins  } from 'vue-chartjs'
+    import { Bar } from 'vue-chartjs'
+    let { reactiveData } = mixins
+
+    export default {
+        extends: Bar,
+        mixins: [reactiveData],
+        data: () => ({
+            chartData: '',
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }),
+        created () {
+            this.fillData()
+        },
+
+        mounted () {
+            this.renderChart(this.chartData, this.options)
+
+            setInterval(() => {
+                this.fillData()
+            }, 5000)
+        },
+
+        methods: {
+            fillData () {
+                this.chartData = {
+                    labels: ['January' + this.getRandomInt(), 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                    datasets: [
+                        {
+                            label: 'Data One',
+                            backgroundColor: '#f87979',
+                            data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+                        }
+                    ]
+                }
+            },
+
+            getRandomInt () {
+                return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+            }
+        }
+    }
+</script>
