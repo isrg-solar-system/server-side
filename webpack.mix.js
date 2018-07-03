87c0f91537126ae740efdde297c19d624dcc8ea7
Andrew Liao <img21326@gmail.com>
2018-07-03 12:46:38 +0800

    asd

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,11 +13,11 @@ let mix = require('laravel-mix');
 
 
 mix
-    // .js('resources/assets/js/inverter.js', 'public/js')
-    // .js('resources/assets/js/front.js', 'public/js')
-    // .js('resources/assets/js/download.js', 'public/js')
-    // .js('resources/assets/js/report.js', 'public/js')
-    // .js('resources/assets/js/back.js', 'public/js')
-    // .js('resources/assets/js/member.js', 'public/js')
-    // .js('resources/assets/js/websetting.js', 'public/js')
-    // .sass('resources/assets/sass/app.scss', 'public/css');
+    .js('resources/assets/js/inverter.js', 'public/js')
+    .js('resources/assets/js/front.js', 'public/js')
+    .js('resources/assets/js/download.js', 'public/js')
+    .js('resources/assets/js/report.js', 'public/js')
+    .js('resources/assets/js/back.js', 'public/js')
+    .js('resources/assets/js/member.js', 'public/js')
+    .js('resources/assets/js/websetting.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
